b11f70e11d21b855d40a266b742cc8ba2def18b2
Max Henkel <max@maxhenkel.de>
2021-12-02 09:17:12 +0100

    Update Forge to 1.18

--- forge/changelog.md
+++ forge/changelog.md
@@ -1,1 +1,1 @@
-- Fixed sounds below Y=0 not being processed
+- Updated to 1.18
--- forge/src/main/java/com/sonicether/soundphysics/ForgeSoundPhysicsMod.java
+++ forge/src/main/java/com/sonicether/soundphysics/ForgeSoundPhysicsMod.java
@@ -3,6 +3,7 @@ package com.sonicether.soundphysics;
 import com.sonicether.soundphysics.config.SoundPhysicsConfig;
 import com.sonicether.soundphysics.integration.ClothConfigIntegration;
 import de.maxhenkel.configbuilder.ConfigBuilder;
+import net.minecraftforge.client.ConfigGuiHandler;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -10,7 +11,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLLoader;
-import net.minecraftforge.fmlclient.ConfigGuiHandler;
 
 import java.nio.file.Path;
 

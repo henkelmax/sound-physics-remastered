367da54124f954d6a7bbe46fc6481ffff9f3f621
Max Henkel <max@maxhenkel.de>
2023-08-24 19:56:59 +0200

    Reworked reflectivity and occlusion config
    (cherry picked from commit 6f797e82d7f597aca83ed300930b6b09254456a1)

--- common/src/main/java/com/sonicether/soundphysics/config/OcclusionConfig.java
+++ common/src/main/java/com/sonicether/soundphysics/config/OcclusionConfig.java
@@ -1,112 +1,57 @@
 package com.sonicether.soundphysics.config;
 
-import com.sonicether.soundphysics.SoundPhysics;
 import com.sonicether.soundphysics.SoundPhysicsMod;
-import de.maxhenkel.configbuilder.CommentedProperties;
-import de.maxhenkel.configbuilder.CommentedPropertyConfig;
+import com.sonicether.soundphysics.config.blocksound.BlockDefinition;
+import com.sonicether.soundphysics.config.blocksound.BlockSoundConfigBase;
 import net.minecraft.world.level.block.SoundType;
 
-import java.io.IOException;
 import java.nio.file.Path;
-import java.util.HashMap;
 import java.util.Map;
 
-public class OcclusionConfig extends CommentedPropertyConfig {
-
-    private Map<SoundType, Float> occlusion;
+public class OcclusionConfig extends BlockSoundConfigBase {
 
     public OcclusionConfig(Path path) {
-        super(new CommentedProperties(false));
-        this.path = path;
-        reload();
+        super(path);
     }
 
     @Override
-    public void load() throws IOException {
-        super.load();
-
-        occlusion = createDefaultMap();
-
-        for (Map.Entry<String, String> entry : properties.entrySet()) {
-            String key = entry.getKey();
-            float value;
-            try {
-                value = Float.parseFloat(entry.getValue());
-            } catch (NumberFormatException e) {
-                SoundPhysics.LOGGER.warn("Failed to parse occlusion factor of {}", key);
-                continue;
-            }
-            SoundType soundType = SoundTypes.getSoundType(key);
-            if (soundType == null) {
-                SoundPhysics.LOGGER.warn("Sound type {} not found", key);
-                continue;
-            }
-
-            occlusion.put(soundType, value);
-        }
-        saveSync();
-    }
-
-    @Override
-    public void saveSync() {
-        properties.clear();
-
-        for (Map.Entry<SoundType, Float> entry : occlusion.entrySet()) {
-            properties.set(SoundTypes.getName(entry.getKey()), String.valueOf(entry.getValue()));
-        }
-
-        super.saveSync();
-    }
-
-    public Map<SoundType, Float> getOcclusionFactors() {
-        return occlusion;
-    }
-
-    public double getOcclusionFactor(SoundType soundType) {
-        return occlusion.getOrDefault(soundType, SoundPhysicsMod.CONFIG.defaultBlockOcclusionFactor.get());
-    }
-
-    public OcclusionConfig setOcclusionFactor(SoundType soundType, float value) {
-        occlusion.put(soundType, value);
-        return this;
-    }
-
-    public Map<SoundType, Float> createDefaultMap() {
-        Map<SoundType, Float> map = new HashMap<>();
+    public void addDefaults(Map<BlockDefinition, Float> map) {
         for (SoundType type : SoundTypes.getTranslationMap().keySet()) {
-            map.put(type, SoundPhysicsMod.CONFIG.defaultBlockOcclusionFactor.get());
+            putSoundType(map, type, SoundPhysicsMod.CONFIG.defaultBlockOcclusionFactor.get());
         }
 
-        map.put(SoundType.WOOL, 1.5F);
-        map.put(SoundType.MOSS, 0.75F);
-        map.put(SoundType.HONEY_BLOCK, 0.5F);
-        map.put(SoundType.GLASS, 0.1F);
-        map.put(SoundType.SNOW, 0.1F);
-        map.put(SoundType.POWDER_SNOW, 0.1F);
-        map.put(SoundType.BAMBOO, 0.1F);
-        map.put(SoundType.BAMBOO_SAPLING, 0.1F);
-        map.put(SoundType.WET_GRASS, 0.1F);
-        map.put(SoundType.MOSS_CARPET, 0.1F);
-        map.put(SoundType.WEEPING_VINES, 0F);
-        map.put(SoundType.TWISTING_VINES, 0F);
-        map.put(SoundType.VINE, 0F);
-        map.put(SoundType.SWEET_BERRY_BUSH, 0F);
-        map.put(SoundType.SPORE_BLOSSOM, 0F);
-        map.put(SoundType.SMALL_DRIPLEAF, 0F);
-        map.put(SoundType.ROOTS, 0F);
-        map.put(SoundType.POINTED_DRIPSTONE, 0F);
-        map.put(SoundType.SCAFFOLDING, 0F);
-        map.put(SoundType.GLOW_LICHEN, 0F);
-        map.put(SoundType.CROP, 0F);
-        map.put(SoundType.FUNGUS, 0F);
-        map.put(SoundType.LILY_PAD, 0F);
-        map.put(SoundType.LARGE_AMETHYST_BUD, 0F);
-        map.put(SoundType.MEDIUM_AMETHYST_BUD, 0F);
-        map.put(SoundType.SMALL_AMETHYST_BUD, 0F);
-        map.put(SoundType.LADDER, 0F);
-        map.put(SoundType.CHAIN, 0F);
-
-        return map;
+        putSoundType(map, SoundType.WOOL, 1.5F);
+        putSoundType(map, SoundType.MOSS, 0.75F);
+        putSoundType(map, SoundType.HONEY_BLOCK, 0.5F);
+        putSoundType(map, SoundType.GLASS, 0.1F);
+        putSoundType(map, SoundType.SNOW, 0.1F);
+        putSoundType(map, SoundType.POWDER_SNOW, 0.1F);
+        putSoundType(map, SoundType.BAMBOO, 0.1F);
+        putSoundType(map, SoundType.BAMBOO_SAPLING, 0.1F);
+        putSoundType(map, SoundType.WET_GRASS, 0.1F);
+        putSoundType(map, SoundType.MOSS_CARPET, 0.1F);
+        putSoundType(map, SoundType.WEEPING_VINES, 0F);
+        putSoundType(map, SoundType.TWISTING_VINES, 0F);
+        putSoundType(map, SoundType.VINE, 0F);
+        putSoundType(map, SoundType.SWEET_BERRY_BUSH, 0F);
+        putSoundType(map, SoundType.SPORE_BLOSSOM, 0F);
+        putSoundType(map, SoundType.SMALL_DRIPLEAF, 0F);
+        putSoundType(map, SoundType.ROOTS, 0F);
+        putSoundType(map, SoundType.POINTED_DRIPSTONE, 0F);
+        putSoundType(map, SoundType.SCAFFOLDING, 0F);
+        putSoundType(map, SoundType.GLOW_LICHEN, 0F);
+        putSoundType(map, SoundType.CROP, 0F);
+        putSoundType(map, SoundType.FUNGUS, 0F);
+        putSoundType(map, SoundType.LILY_PAD, 0F);
+        putSoundType(map, SoundType.LARGE_AMETHYST_BUD, 0F);
+        putSoundType(map, SoundType.MEDIUM_AMETHYST_BUD, 0F);
+        putSoundType(map, SoundType.SMALL_AMETHYST_BUD, 0F);
+        putSoundType(map, SoundType.LADDER, 0F);
+        putSoundType(map, SoundType.CHAIN, 0F);
     }
 
+    @Override
+    public Float getDefaultValue() {
+        return SoundPhysicsMod.CONFIG.defaultBlockOcclusionFactor.get();
+    }
 }
--- common/src/main/java/com/sonicether/soundphysics/config/ReflectivityConfig.java
+++ common/src/main/java/com/sonicether/soundphysics/config/ReflectivityConfig.java
@@ -1,114 +1,59 @@
 package com.sonicether.soundphysics.config;
 
-import com.sonicether.soundphysics.SoundPhysics;
 import com.sonicether.soundphysics.SoundPhysicsMod;
-import de.maxhenkel.configbuilder.CommentedProperties;
-import de.maxhenkel.configbuilder.CommentedPropertyConfig;
+import com.sonicether.soundphysics.config.blocksound.BlockDefinition;
+import com.sonicether.soundphysics.config.blocksound.BlockSoundConfigBase;
 import net.minecraft.world.level.block.SoundType;
 
-import java.io.IOException;
 import java.nio.file.Path;
-import java.util.HashMap;
 import java.util.Map;
 
-public class ReflectivityConfig extends CommentedPropertyConfig {
-
-    private Map<SoundType, Float> reflectivity;
+public class ReflectivityConfig extends BlockSoundConfigBase {
 
     public ReflectivityConfig(Path path) {
-        super(new CommentedProperties(false));
-        this.path = path;
-        reload();
+        super(path);
     }
 
     @Override
-    public void load() throws IOException {
-        super.load();
-
-        reflectivity = createDefaultMap();
-
-        for (Map.Entry<String, String> entry : properties.entrySet()) {
-            String key = entry.getKey();
-            float value;
-            try {
-                value = Float.parseFloat(entry.getValue());
-            } catch (NumberFormatException e) {
-                SoundPhysics.LOGGER.warn("Failed to parse reflectivity of {}", key);
-                continue;
-            }
-            SoundType soundType = SoundTypes.getSoundType(key);
-            if (soundType == null) {
-                SoundPhysics.LOGGER.warn("Sound type {} not found", key);
-                continue;
-            }
-
-            reflectivity.put(soundType, value);
-        }
-        saveSync();
-    }
-
-    @Override
-    public void saveSync() {
-        properties.clear();
-
-        for (Map.Entry<SoundType, Float> entry : reflectivity.entrySet()) {
-            properties.set(SoundTypes.getName(entry.getKey()), String.valueOf(entry.getValue()));
-        }
-
-        super.saveSync();
-    }
-
-    public Map<SoundType, Float> getReflectivities() {
-        return reflectivity;
-    }
-
-    public double getReflectivity(SoundType soundType) {
-        return reflectivity.getOrDefault(soundType, SoundPhysicsMod.CONFIG.defaultBlockReflectivity.get());
-    }
-
-    public ReflectivityConfig setReflectivity(SoundType soundType, float value) {
-        reflectivity.put(soundType, value);
-        return this;
-    }
-
-    public Map<SoundType, Float> createDefaultMap() {
-        Map<SoundType, Float> map = new HashMap<>();
+    public void addDefaults(Map<BlockDefinition, Float> map) {
         for (SoundType type : SoundTypes.getTranslationMap().keySet()) {
-            map.put(type, SoundPhysicsMod.CONFIG.defaultBlockReflectivity.get());
+            putSoundType(map, type, SoundPhysicsMod.CONFIG.defaultBlockReflectivity.get());
         }
 
-        map.put(SoundType.STONE, 1.5F);
-        map.put(SoundType.NETHERITE_BLOCK, 1.5F);
-        map.put(SoundType.TUFF, 1.5F);
-        map.put(SoundType.AMETHYST, 1.5F);
-        map.put(SoundType.BASALT, 1.5F);
-        map.put(SoundType.CALCITE, 1.5F);
-        map.put(SoundType.BONE_BLOCK, 1.5F);
-        map.put(SoundType.COPPER, 1.25F);
-        map.put(SoundType.DEEPSLATE, 1.5F);
-        map.put(SoundType.DEEPSLATE_BRICKS, 1.5F);
-        map.put(SoundType.DEEPSLATE_TILES, 1.5F);
-        map.put(SoundType.POLISHED_DEEPSLATE, 1.5F);
-        map.put(SoundType.NETHER_BRICKS, 1.5F);
-        map.put(SoundType.NETHERRACK, 1.1F);
-        map.put(SoundType.NETHER_GOLD_ORE, 1.1F);
-        map.put(SoundType.NETHER_ORE, 1.1F);
-        map.put(SoundType.STEM, 0.4F);
-        map.put(SoundType.WOOL, 0.1F);
-        map.put(SoundType.HONEY_BLOCK, 0.1F);
-        map.put(SoundType.MOSS, 0.1F);
-        map.put(SoundType.SOUL_SAND, 0.2F);
-        map.put(SoundType.SOUL_SOIL, 0.2F);
-        map.put(SoundType.CORAL_BLOCK, 0.2F);
-        map.put(SoundType.METAL, 1.25F);
-        map.put(SoundType.WOOD, 0.4F);
-        map.put(SoundType.GRAVEL, 0.3F);
-        map.put(SoundType.GRASS, 0.3F);
-        map.put(SoundType.GLASS, 0.75F);
-        map.put(SoundType.SAND, 0.2F);
-        map.put(SoundType.SNOW, 0.15F);
-
-        return map;
+        putSoundType(map, SoundType.STONE, 1.5F);
+        putSoundType(map, SoundType.NETHERITE_BLOCK, 1.5F);
+        putSoundType(map, SoundType.TUFF, 1.5F);
+        putSoundType(map, SoundType.AMETHYST, 1.5F);
+        putSoundType(map, SoundType.BASALT, 1.5F);
+        putSoundType(map, SoundType.CALCITE, 1.5F);
+        putSoundType(map, SoundType.BONE_BLOCK, 1.5F);
+        putSoundType(map, SoundType.COPPER, 1.25F);
+        putSoundType(map, SoundType.DEEPSLATE, 1.5F);
+        putSoundType(map, SoundType.DEEPSLATE_BRICKS, 1.5F);
+        putSoundType(map, SoundType.DEEPSLATE_TILES, 1.5F);
+        putSoundType(map, SoundType.POLISHED_DEEPSLATE, 1.5F);
+        putSoundType(map, SoundType.NETHER_BRICKS, 1.5F);
+        putSoundType(map, SoundType.NETHERRACK, 1.1F);
+        putSoundType(map, SoundType.NETHER_GOLD_ORE, 1.1F);
+        putSoundType(map, SoundType.NETHER_ORE, 1.1F);
+        putSoundType(map, SoundType.STEM, 0.4F);
+        putSoundType(map, SoundType.WOOL, 0.1F);
+        putSoundType(map, SoundType.HONEY_BLOCK, 0.1F);
+        putSoundType(map, SoundType.MOSS, 0.1F);
+        putSoundType(map, SoundType.SOUL_SAND, 0.2F);
+        putSoundType(map, SoundType.SOUL_SOIL, 0.2F);
+        putSoundType(map, SoundType.CORAL_BLOCK, 0.2F);
+        putSoundType(map, SoundType.METAL, 1.25F);
+        putSoundType(map, SoundType.WOOD, 0.4F);
+        putSoundType(map, SoundType.GRAVEL, 0.3F);
+        putSoundType(map, SoundType.GRASS, 0.3F);
+        putSoundType(map, SoundType.GLASS, 0.75F);
+        putSoundType(map, SoundType.SAND, 0.2F);
+        putSoundType(map, SoundType.SNOW, 0.15F);
     }
 
+    @Override
+    public Float getDefaultValue() {
+        return SoundPhysicsMod.CONFIG.defaultBlockReflectivity.get();
+    }
 }

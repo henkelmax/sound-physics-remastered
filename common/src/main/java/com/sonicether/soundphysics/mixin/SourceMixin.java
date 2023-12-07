8d345bbbc67db557d3d055e4d8db51ce13200f12
Max Henkel <max@maxhenkel.de>
2023-12-07 11:07:51 +0100

    Fix reverb not working Closes #178
    (cherry picked from commit 8428489c27aab0001eb7010bb7b57bae6100a52b)

--- common/src/main/java/com/sonicether/soundphysics/mixin/SourceMixin.java
+++ common/src/main/java/com/sonicether/soundphysics/mixin/SourceMixin.java
@@ -5,13 +5,13 @@ import com.sonicether.soundphysics.Loggers;
 import com.sonicether.soundphysics.SoundPhysics;
 import com.sonicether.soundphysics.SoundPhysicsMod;
 import net.minecraft.world.phys.Vec3;
-import org.lwjgl.openal.AL11;
+import org.lwjgl.openal.AL10;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Channel.class)
@@ -34,15 +34,17 @@ public class SourceMixin {
         Loggers.logALError("Sound play injector");
     }
 
-    @ModifyArg(method = "linearAttenuation", at = @At(value = "INVOKE", target = "org/lwjgl/openal/AL10.alSourcef(IIF)V", ordinal = 0, remap = false), index = 2)
-    private float linearAttenuation(int pointer, int param, float attenuation) {
+    @ModifyVariable(method = "linearAttenuation", at = @At("HEAD"), ordinal = 0, argsOnly = true)
+    private float injected(float attenuation) {
         if (!SoundPhysicsMod.CONFIG.enabled.get()) {
             return attenuation;
         }
-        if (param != AL11.AL_MAX_DISTANCE) {
-            throw new IllegalArgumentException("Tried modifying wrong field. No attenuation here.");
-        }
         return attenuation / SoundPhysicsMod.CONFIG.attenuationFactor.get();
+    }
+
+    @Inject(method = "linearAttenuation", at = @At("RETURN"))
+    private void linearAttenuation2(float attenuation, CallbackInfo ci) {
+        AL10.alSourcef(source, AL10.AL_REFERENCE_DISTANCE, attenuation / 2F);
     }
 
 }

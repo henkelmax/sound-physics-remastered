f6c5b101f556719eb3ab61b36c6b9396db5dd4ae
Max Henkel <max@maxhenkel.de>
2023-12-07 14:26:46 +0100

    Fix boat rowing sounds being muffled Closes #138

---
+++ common/src/main/java/com/sonicether/soundphysics/mixin/ClientLevelMixin.java
@@ -0,0 +1,34 @@
+package com.sonicether.soundphysics.mixin;
+
+import com.sonicether.soundphysics.SoundPhysicsMod;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.core.Holder;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientLevel.class)
+public abstract class ClientLevelMixin {
+
+    @Inject(method = "playSeededSound(Lnet/minecraft/world/entity/player/Player;DDDLnet/minecraft/core/Holder;Lnet/minecraft/sounds/SoundSource;FFJ)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;playSound(DDDLnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FFZJ)V"), cancellable = true)
+    private void playSeededSound(Player player, double x, double y, double z, Holder<SoundEvent> holder, SoundSource soundSource, float g, float h, long l, CallbackInfo ci) {
+        if (!SoundPhysicsMod.CONFIG.enabled.get()) {
+            return;
+        }
+        SoundEvent value = holder.value();
+        if (SoundEvents.BOAT_PADDLE_WATER.equals(value)) {
+            ci.cancel();
+            playSound(x, y + 0.5D, z, value, soundSource, g, h, false, l);
+        }
+    }
+
+    @Shadow
+    protected abstract void playSound(double d, double e, double f, SoundEvent soundEvent, SoundSource soundSource, float g, float h, boolean bl, long l);
+
+}

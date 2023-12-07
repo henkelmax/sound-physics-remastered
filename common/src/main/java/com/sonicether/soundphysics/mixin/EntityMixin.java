1ba6323b3c183d52f21d1e1594412711daa6d603
Max Henkel <max@maxhenkel.de>
2023-12-07 14:52:55 +0100

    Fix player hurt sound muffled when standing in water Closes #137
    (cherry picked from commit 191cb47c93a2a63023ae2e8dbafda3b8123cd6aa)

--- common/src/main/java/com/sonicether/soundphysics/mixin/EntityMixin.java
+++ common/src/main/java/com/sonicether/soundphysics/mixin/EntityMixin.java
@@ -1,24 +1,20 @@
 package com.sonicether.soundphysics.mixin;
 
 import com.sonicether.soundphysics.SoundPhysicsMod;
+import com.sonicether.soundphysics.utils.SoundUtils;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-import java.util.regex.Pattern;
-
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
-
 import javax.annotation.Nullable;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
-
-    private static final Pattern STEP_PATTERN = Pattern.compile(".*step.*");
 
     @Shadow
     public abstract float getEyeHeight();
@@ -31,13 +27,7 @@ public abstract class EntityMixin {
         if (!SoundPhysicsMod.CONFIG.enabled.get()) {
             return y;
         }
-        return y + calculateEntitySoundOffset(getEyeHeight(), sound);
+        return y + SoundUtils.calculateEntitySoundYOffset(getEyeHeight(), sound);
     }
 
-    private static double calculateEntitySoundOffset(float standingEyeHeight, SoundEvent sound) {
-        if (STEP_PATTERN.matcher(sound.getLocation().getPath()).matches()) {
-            return 0D;
-        }
-        return standingEyeHeight;
-    }
 }

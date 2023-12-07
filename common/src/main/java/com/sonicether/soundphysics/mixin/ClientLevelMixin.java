e624954928605b5d4fde724778fa07ab62fb7234
Max Henkel <max@maxhenkel.de>
2023-12-07 20:20:42 +0100

    Backport changes to 1.19.2

--- common/src/main/java/com/sonicether/soundphysics/mixin/ClientLevelMixin.java
+++ common/src/main/java/com/sonicether/soundphysics/mixin/ClientLevelMixin.java
@@ -2,7 +2,6 @@ package com.sonicether.soundphysics.mixin;
 
 import com.sonicether.soundphysics.SoundPhysicsMod;
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.core.Holder;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -16,15 +15,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientLevel.class)
 public abstract class ClientLevelMixin {
 
-    @Inject(method = "playSeededSound(Lnet/minecraft/world/entity/player/Player;DDDLnet/minecraft/core/Holder;Lnet/minecraft/sounds/SoundSource;FFJ)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;playSound(DDDLnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FFZJ)V"), cancellable = true)
-    private void playSeededSound(Player player, double x, double y, double z, Holder<SoundEvent> holder, SoundSource soundSource, float g, float h, long l, CallbackInfo ci) {
+    @Inject(method = "playSeededSound(Lnet/minecraft/world/entity/player/Player;DDDLnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FFJ)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;playSound(DDDLnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FFZJ)V"), cancellable = true)
+    private void playSeededSound(Player player, double x, double y, double z, SoundEvent soundEvent, SoundSource soundSource, float g, float h, long l, CallbackInfo ci) {
         if (!SoundPhysicsMod.CONFIG.enabled.get()) {
             return;
         }
-        SoundEvent value = holder.value();
-        if (SoundEvents.BOAT_PADDLE_WATER.equals(value)) {
+        if (SoundEvents.BOAT_PADDLE_WATER.equals(soundEvent)) {
             ci.cancel();
-            playSound(x, y + 0.5D, z, value, soundSource, g, h, false, l);
+            playSound(x, y + 0.5D, z, soundEvent, soundSource, g, h, false, l);
         }
     }
 

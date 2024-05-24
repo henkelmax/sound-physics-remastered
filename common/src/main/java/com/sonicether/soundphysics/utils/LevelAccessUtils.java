4726705d0b4264f7f8621eb38f6108797f7d7742
Max Henkel <max@maxhenkel.de>
2024-05-24 22:41:41 +0200

    Make level clone values configurable (#211)

--- common/src/main/java/com/sonicether/soundphysics/utils/LevelAccessUtils.java
+++ common/src/main/java/com/sonicether/soundphysics/utils/LevelAccessUtils.java
@@ -1,6 +1,7 @@
 package com.sonicether.soundphysics.utils;
 
 import com.sonicether.soundphysics.Loggers;
+import com.sonicether.soundphysics.SoundPhysicsMod;
 import com.sonicether.soundphysics.profiling.TaskProfiler;
 import com.sonicether.soundphysics.world.CachingClientLevel;
 import com.sonicether.soundphysics.world.ClientLevelProxy;
@@ -14,57 +15,50 @@ import net.minecraft.world.entity.player.Player;
 
 /**
  * Utility module to manage creation, invalidation, and updating of client level clones.
- * 
+ * <p>
  * Level clones are created on a client tick basis and retained for some time.
  * Any module on any thread may access the cached level clone for read-only world operations.
- * 
+ *
  * @author Saint (@augustsaintfreytag)
  */
 public class LevelAccessUtils {
-
-    // Configuration
-
-    private static final boolean USE_UNSAFE_LEVEL_ACCESS = false;           // Disable level clone and cache and fall back to original main thread access. (Default: false)
-    private static final int LEVEL_CLONE_RANGE = 4;                         // Cloned number of chunks in radius around player position. (Default: 4 chunks)
-    private static final long LEVEL_CLONE_MAX_RETAIN_TICKS = 20;            // Maximum number of ticks to retain level clone in cache. (Default: 20 ticks / 1 second)
-    private static final long LEVEL_CLONE_MAX_RETAIN_BLOCK_DISTANCE = 16;   // Maximum distance player can move from cloned origin before invalidation. (Default: 25% clone radius)
 
     private static final TaskProfiler profiler = new TaskProfiler("Level Caching");
 
     // Cache Write
 
     public static void tickLevelCache(ClientLevel clientLevel, Player player) {
-        if (USE_UNSAFE_LEVEL_ACCESS) {
+        if (SoundPhysicsMod.CONFIG.unsafeLevelAccess.get()) {
             // Disable all level cloning, use direct unsafe main thread access (original behavior).
             return;
         }
 
         var currentTick = clientLevel.getGameTime();
         var origin = levelOriginFromPlayer(player);
-        
+
         // Cast client level reference to interface to access injected level cache property.
         var cachingClientLevel = (CachingClientLevel) (Object) clientLevel;
         var clientLevelClone = cachingClientLevel.getCachedClone();
-        
+
         if (clientLevelClone == null) {
             // No cache exists, cache first level clone.
 
             Loggers.logDebug("Creating new level cache, no existing level clone found in client cache.");
-            updateLevelCache(clientLevel, origin, LEVEL_CLONE_MAX_RETAIN_TICKS);
+            updateLevelCache(clientLevel, origin, SoundPhysicsMod.CONFIG.levelCloneMaxRetainTicks.get());
             return;
         }
 
         var ticksSinceLastClone = currentTick - clientLevelClone.getTick();
         var distanceSinceLastClone = origin.distSqr(clientLevelClone.getOrigin());
 
-        if (ticksSinceLastClone >= LEVEL_CLONE_MAX_RETAIN_TICKS || distanceSinceLastClone >= LEVEL_CLONE_MAX_RETAIN_BLOCK_DISTANCE) {
+        if (ticksSinceLastClone >= SoundPhysicsMod.CONFIG.levelCloneMaxRetainTicks.get() || distanceSinceLastClone >= SoundPhysicsMod.CONFIG.levelCloneMaxRetainBlockDistance.get()) {
             // Cache expired or player travelled too far from last clone origin point, update cache.
-            
+
             Loggers.logDebug(
-                "Updating level cache, cache expired ({}/{} ticks) or player moved too far ({}/{} block(s)) from last clone origin.", 
-                ticksSinceLastClone, LEVEL_CLONE_MAX_RETAIN_TICKS, distanceSinceLastClone, LEVEL_CLONE_MAX_RETAIN_BLOCK_DISTANCE
+                    "Updating level cache, cache expired ({}/{} ticks) or player moved too far ({}/{} block(s)) from last clone origin.",
+                    ticksSinceLastClone, SoundPhysicsMod.CONFIG.levelCloneMaxRetainTicks.get(), distanceSinceLastClone, SoundPhysicsMod.CONFIG.levelCloneMaxRetainBlockDistance.get()
             );
-            
+
             updateLevelCache(clientLevel, origin, currentTick);
         }
     }
@@ -74,7 +68,7 @@ public class LevelAccessUtils {
 
         var profile = profiler.profile();
         var cachingClientLevel = (CachingClientLevel) (Object) clientLevel;
-        var clientLevelClone = new ClonedClientLevel(clientLevel, origin, tick, LEVEL_CLONE_RANGE);
+        var clientLevelClone = new ClonedClientLevel(clientLevel, origin, tick, SoundPhysicsMod.CONFIG.levelCloneRange.get());
 
         cachingClientLevel.setCachedClone(clientLevelClone);
 
@@ -94,7 +88,7 @@ public class LevelAccessUtils {
             return null;
         }
 
-        if (USE_UNSAFE_LEVEL_ACCESS) {
+        if (SoundPhysicsMod.CONFIG.unsafeLevelAccess.get()) {
             return new UnsafeClientLevel(clientLevel);
         }
 

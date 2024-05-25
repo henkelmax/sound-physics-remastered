ce248d5cf993738182ebb09ea2dfc2b088c8eec1
Max Henkel <max@maxhenkel.de>
2024-05-25 21:48:10 +0200

    Fix audio positioning when eating or drinking Closes #202
    (cherry picked from commit adb5313f294bd0484dce5a98932514e0c44626a7)

--- common/src/main/java/com/sonicether/soundphysics/mixin/LocalPlayerMixin.java
+++ common/src/main/java/com/sonicether/soundphysics/mixin/LocalPlayerMixin.java
@@ -1,16 +1,15 @@
 package com.sonicether.soundphysics.mixin;
 
 import com.sonicether.soundphysics.SoundPhysicsMod;
-import com.sonicether.soundphysics.utils.SoundUtils;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LocalPlayer.class)
 public abstract class LocalPlayerMixin extends Entity {
@@ -19,15 +18,13 @@ public abstract class LocalPlayerMixin extends Entity {
         super(entityType, level);
     }
 
-    @ModifyArg(method = "playSound", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;playLocalSound(DDDLnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FFZ)V"), index = 1)
-    private double playSound(double x, double y, double z, SoundEvent sound, SoundSource category, float volume, float pitch, boolean b) {
-        if (sound == null) {
-            return y;
-        }
+    @Inject(method = "playSound", at = @At("HEAD"), cancellable = true)
+    private void playSound(SoundEvent soundEvent, float volume, float pitch, CallbackInfo ci) {
         if (!SoundPhysicsMod.CONFIG.enabled.get()) {
-            return y;
+            return;
         }
-        return y + SoundUtils.calculateEntitySoundYOffset(getEyeHeight(), sound);
+        ci.cancel();
+        level().playLocalSound(this, soundEvent, getSoundSource(), volume, pitch);
     }
 
 }

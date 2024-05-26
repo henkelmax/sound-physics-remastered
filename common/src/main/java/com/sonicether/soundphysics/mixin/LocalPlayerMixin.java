ebcac92d9aa72a0fbfe0b248c5bdafbad04a9c26
Max Henkel <max@maxhenkel.de>
2024-05-26 12:14:02 +0200

    Backport changes to 1.20.1

--- common/src/main/java/com/sonicether/soundphysics/mixin/LocalPlayerMixin.java
+++ common/src/main/java/com/sonicether/soundphysics/mixin/LocalPlayerMixin.java
@@ -24,7 +24,7 @@ public abstract class LocalPlayerMixin extends Entity {
             return;
         }
         ci.cancel();
-        level().playLocalSound(this, soundEvent, getSoundSource(), volume, pitch);
+        level().playSound((LocalPlayer) (Object) this, this, soundEvent, getSoundSource(), volume, pitch);
     }
 
 }

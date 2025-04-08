0994d0038e9c6602de0a83358f74eed55c1ea982
Max Henkel <max@maxhenkel.de>
2025-04-08 17:56:57 +0200

    Fix mixin error on Forge Closes #284

--- common/src/main/java/com/sonicether/soundphysics/mixin/LibraryMixin.java
+++ common/src/main/java/com/sonicether/soundphysics/mixin/LibraryMixin.java
@@ -1,13 +1,13 @@
 package com.sonicether.soundphysics.mixin;
 
 import com.mojang.blaze3d.audio.Library;
+import org.lwjgl.openal.ALC10;
 import org.lwjgl.openal.EXTEfx;
 import org.lwjgl.system.MemoryStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.nio.IntBuffer;
 import java.util.Arrays;
@@ -15,9 +15,8 @@ import java.util.Arrays;
 @Mixin(value = Library.class)
 public class LibraryMixin {
 
-    @ModifyArgs(method = "init", at = @At(value = "INVOKE", target = "Lorg/lwjgl/openal/ALC10;alcCreateContext(JLjava/nio/IntBuffer;)J"))
-    private void modifyContext(Args args) {
-        IntBuffer oldBuffer = args.get(1);
+    @Redirect(method = "init", at = @At(value = "INVOKE", target = "Lorg/lwjgl/openal/ALC10;alcCreateContext(JLjava/nio/IntBuffer;)J"))
+    private long injected(long deviceHandle, IntBuffer oldBuffer) {
         try (MemoryStack stack = MemoryStack.stackPush()) {
             IntBuffer buffer;
             if (oldBuffer != null) {
@@ -28,7 +27,7 @@ public class LibraryMixin {
                 buffer = stack.mallocInt(4);
             }
             buffer.put(EXTEfx.ALC_MAX_AUXILIARY_SENDS).put(4).put(0);
-            args.set(1, buffer.put(0).flip());
+            return ALC10.alcCreateContext(deviceHandle, buffer.put(0).flip());
         }
     }
 
